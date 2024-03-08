0d88a26929697d80c41e0d666b50da3372eed727
Graeme Gillies <graeme.a.gillies@gmail.com>
2024-03-08 13:45:07 +0000

    fixed vercel deplyment bug

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import NavBar from "@/components/Navbar";
+import NavBar from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
