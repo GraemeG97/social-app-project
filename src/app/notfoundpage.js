4526e2f423a77102cc19bfb77fb2b432c9664967
Graeme Gillies <graeme.a.gillies@gmail.com>
2024-03-11 10:01:25 +0000

    not found page

---
+++ src/app/notfoundpage.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Page Not Found</h2>
+      <p>We cannot find this page requested.</p>
+      <Link href="/">Return Home</Link>
+    </div>
+  );
+}
